cd12ddbcf47b5e67e8f9c5102b07db0990a7ee38
Emmanuel Nyachoke <achachiez@gmail.com>
2018-02-28 14:37:35 +0300

    Added initial where clause,group by,order by and paging

--- src/sql-generators.js
+++ src/sql-generators.js
@@ -4,17 +4,74 @@ export default class SqlGenerators {
   }
   generateColumns(select, columns) {
     for (let column of columns) {
-      if (column.type == 'column') {
+      if (column.type === 'column') {
         select.field(column.dataSetColumn, column.alias);
       } else {
-        console.log('Name',column.expression);
+        console.log('Name', column.expression);
         select.field(column.expression, column.alias);
       }
 
     }
     return select.toString();
   }
-  generateWhere() {
-    return [1, 3, 3];
+  generateWhere(select, filters, params) {
+    for (let condition of filters.conditions) {
+      select.where(condition.conditionExpession, params[condition.parameteName])
+    }
+    return select.toString();
   }
+
+  generateGroupBy(select, groupBy, params) {
+    let groupParams = params[groupBy.groupParam] || [];
+    if (groupParams.length > 0) {
+      return this.addGroupColumns(select, groupParams);
+    } else {
+      return this.addGroupColumns(select, groupBy.columns);
+    }
+  }
+
+
+  generateOrderBy(select, orderBy, params) {
+    let orderParams = params[orderBy.orderByParam] || [];
+    if (orderParams.length > 0) {
+      return this.addOrderColumns(select, orderParams);
+    } else {
+      return this.addOrderColumns(select, orderBy.columns);
+    }
+  }
+
+  generatePaging(select, paging, params) {
+    if (paging) {
+      let limit = params[paging.limitParam];
+      let offset = params[paging.offSetParam];
+      if(limit >= 0){
+        select.limit(limit);
+      }
+      if(offset >= 0){
+      select.offset(offset);
+      }
+      return select.toString();
+    } else {
+      return select.toString();
+    }
+  }
+
+  addGroupColumns(select, columns) {
+    for (let column of columns) {
+      select.group(column);
+    }
+    return select.toString();
+  }
+
+  addOrderColumns(select, columns) {
+    for (let column of columns) {
+      let asc = true;
+      if (column.order && column.order.toUpperCase() === 'desc'.toUpperCase()) {
+        asc = false;
+      }
+      select.order(column.column, asc);
+    }
+    return select.toString();
+  }
+
 }
--- test/sql-generators.spec.js
+++ test/sql-generators.spec.js
@@ -5,7 +5,8 @@ chai.use(require('chai-string'));
 chai.expect();
 import {
   SqlGenerators
-} from '../lib/ampath-json2sql';
+}
+from '../src';
 
 const expect = chai.expect;
 const should = chai.should;
@@ -17,7 +18,7 @@ describe('Generate Column Specs', () => {
 
   });
   describe('Should return columns when generateColumns() is called with a columns json object', () => {
-    it('should return the name', () => {
+    it('should return the correct select statement', () => {
       let select = Squel.select();
       let columns = [{
         type: 'column',
@@ -30,7 +31,7 @@ describe('Generate Column Specs', () => {
         expression: `case when age between 0 and 1 then '0_to_1'  else 'older_than_24'  end`
       }];
       expect(generate.generateColumns(select, columns))
-      .equalIgnoreCase(`SELECT p.age as "age", case when age between 0 and 1 then '0_to_1'  else 'older_than_24'  end As "age_range"`)
+        .equalIgnoreCase(`SELECT p.age as "age", case when age between 0 and 1 then '0_to_1'  else 'older_than_24'  end As "age_range"`)
     });
   });
 });
@@ -40,8 +41,126 @@ describe('Generate Where Specs', () => {
     generate = new SqlGenerators();
   });
   describe('Should return where clause when generateWhere() is called with a filters json object', () => {
-    it('should return the name', () => {
-      expect(generate.generateWhere()).contains(1)
+    let select = Squel.select();
+    //TODO add case for expression
+    let filters = {
+      conditionJoinOperator: "and",
+      conditions: [{
+        filterType: "tableColumns",
+        conditionExpession: "endDate = ?",
+        parameteName: 'endDate'
+      }]
+    };
+    it('should return the correct select statement', () => {
+      expect(generate.generateWhere(select, filters, {
+          endDate: '2017-10-10'
+        }))
+        .equalIgnoreCase(`SELECT where (endDate = '2017-10-10')`)
     });
   });
+});
+
+describe('Generate Group by Specs', () => {
+  before(() => {
+    generate = new SqlGenerators();
+  });
+  describe('Should return grou[] clause when generateGroupBy() is called', () => {
+    let groupBy = {
+      groupParam: "groupByParam",
+      columns: ["gender", "age"]
+    };
+    it('should return the correct group by statement', () => {
+      let select = Squel.select();
+      expect(generate.generateGroupBy(select, groupBy, {}))
+        .equalIgnoreCase(`SELECT group by gender, age`)
+    });
+
+    it('It should ignore the report group columns if they are provided in the group params', () => {
+      let select = Squel.select();
+      expect(generate.generateGroupBy(select, groupBy, {
+          groupByParam: ['age']
+        }))
+        .equalIgnoreCase(`SELECT group by age`)
+    });
+  });
+});
+
+describe('Order by Specs', () => {
+  describe('Should return order clause when generateOrderBy() is called ', () => {
+    let orderBy = {
+      orderByParam: "orderByParam",
+      columns: [{
+        column: "gender",
+        order: "asc"
+      }, {
+        column: "age",
+        order: "desc"
+      }]
+    };
+    it('should return the correct order by statement', () => {
+      let select = Squel.select();
+      expect(generate.generateOrderBy(select, orderBy, {}))
+        .equalIgnoreCase(`SELECT order by gender asc, age desc`)
+    });
+
+    it('It should ignore the report order columns if they are provided in the order by params', () => {
+      let select = Squel.select();
+      expect(generate.generateOrderBy(select, orderBy, {
+          orderByParam: [{
+            column: "age",
+            order: "asc"
+          }]
+        }))
+        .equalIgnoreCase(`SELECT order by age asc`)
+    });
+  });
+});
+
+describe('Paging by Specs', () => {
+  describe('Should paginate generatePaging() is called ', () => {
+    it('should not set paging if paging is not defined', () => {
+      let select = Squel.select();
+      expect(generate.generatePaging(select, null, {}))
+        .equalIgnoreSpaces(`SELECT`)
+    });
+
+    it('It should add paging if paging is provided in the report', () => {
+      let paging = {
+        offSetParam: "offSetParam",
+        limitParam: "limitParam"
+      };
+      let select = Squel.select();
+      expect(generate.generatePaging(select, paging, {
+          limitParam: 10,
+          offSetParam: 0
+        }))
+        .equalIgnoreCase(`SELECT limit 10 offset 0`)
+    });
+
+    it('It should ignore the limit if it can not find the param', () => {
+      let select = Squel.select();
+      let paging = {
+        offSetParam: "offSetParam",
+        limitParam: "limitParam"
+      };
+      expect(generate.generatePaging(select, paging, {
+          offSetParam: 0
+        }))
+        .equalIgnoreCase(`SELECT offset 0`)
+    });
+
+    it('It should ignore the offset if it can not find the param', () => {
+      let select = Squel.select();
+      let paging = {
+        offSetParam: "offSetParam",
+        limitParam: "limitParam"
+      };
+      expect(generate.generatePaging(select, paging, {
+          limitParam: 10
+        }))
+        .equalIgnoreCase(`SELECT limit 10`)
+    });
+
+  });
+
 });
